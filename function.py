c87628b862b72dabe3302d204b1f55c87e5b6ee4
Santhosh15890 <santhoshking2697@gmail.com>
2025-09-16 14:22:47 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,22 @@
+def area (length,breadth):
+    return(length*breadth)
+print("Area of rectangle",area(10,20))
+
+
+def greet():
+     name=input("Enter a name")
+     return name
+print("Welcome to coding:",greet())
+
+
+def num():
+    x=int(input("Enter a number"))
+    y=int(input("Enter a number"))
+    print("add:",x+y)
+num()
+
+
+def add(a,b):
+     print("sum",a+b)
+add(5,6)           
+           
