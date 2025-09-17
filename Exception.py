4e388f69f3194d750cd24c942124c4aa3e956239
Santhosh15890 <santhoshking2697@gmail.com>
2025-09-17 17:17:18 +0530

    Add files via upload

---
+++ Exception.py
@@ -0,0 +1,20 @@
+index error
+l=[1,2,3,4,5]
+print(l[7])
+
+type error
+a=25
+b="python"
+print(a+b)
+
+name error
+print(x)
+
+value error
+num=int(input("Enter a number"))
+print(num)
+
+enter a number:abc
+
+filenot found error
+f=open("myfile.txt","r")
